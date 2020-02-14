4d0c3238ea273d4e1064e5864112da9987741a1d
Priscila <priscila.oliveiraa@gmail.com>
2020-02-14 15:46:41 -0500

    Refactored post and put routes to reuse code.

--- server/index.js
+++ server/index.js
@@ -17,9 +17,39 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.engine('html', ejs.renderFile);
 
+const buildNewExp = (req) => {
+  const experienceId = parseInt(req.body.experienceId, 10);
+  const averageRating = parseInt(req.body.averageRating, 10);
+  const numberOfReviews = parseInt(req.body.numberOfReviews, 10);
+  const duration = parseInt(req.body.duration, 10);
+  const groupSize = parseInt(req.body.groupSize, 10);
+  const costPerPerson = parseInt(req.body.costPerPerson, 10);
+  const newExp = {
+    experienceId,
+    title: req.body.title,
+    city: req.body.city,
+    state: req.body.state,
+    country: req.body.country,
+    category: req.body.category,
+    activity: req.body.state,
+    averageRating,
+    numberOfReviews,
+    duration,
+    groupSize,
+    includes: req.body.includes,
+    cuisine: req.body.cuisine,
+    hostedLanguages: req.body.hostedLanguages,
+    costPerPerson,
+    imageUrls: req.body.imageUrls,
+    videoUrl: req.body.videoUrl,
+  };
+  return newExp;
+};
+
 app.get('/:id', (req, res) => {
   res.render('../public/index.html');
 });
+
 
 app.get('/api/experiences/:id', (req, res) => {
   const expId = {
@@ -38,35 +68,7 @@ app.get('/api/experiences/:id', (req, res) => {
 });
 
 app.post('/api/experiences/', (req, res) => {
-  console.log('post here');
-  const experienceId = parseInt(req.body.experienceId, 10);
-  const averageRating = parseInt(req.body.averageRating, 10);
-  const numberOfReviews = parseInt(req.body.numberOfReviews, 10);
-  const duration = parseInt(req.body.duration, 10);
-  const groupSize = parseInt(req.body.groupSize, 10);
-  const costPerPerson = parseInt(req.body.costPerPerson, 10);
-  const newExp = {
-    experienceId,
-    title: req.body.title,
-    city: req.body.city,
-    state: req.body.state,
-    country: req.body.country,
-    category: req.body.category,
-    activity: req.body.state,
-    averageRating,
-    numberOfReviews,
-    duration,
-    groupSize,
-    includes: req.body.includes,
-    cuisine: req.body.cuisine,
-    hostedLanguages: req.body.hostedLanguages,
-    costPerPerson,
-    imageUrls: req.body.imageUrls,
-    videoUrl: req.body.videoUrl,
-  };
-  console.log('newExp server', newExp);
-
-  db.createExperience(newExp, (err, data) => {
+  db.createExperience(buildNewExp(req), (err, data) => {
     if (err) {
       res.status(400).send({
         message: err.message,
@@ -78,33 +80,7 @@ app.post('/api/experiences/', (req, res) => {
 });
 
 app.put('/api/experiences/:id', (req, res) => {
-  const experienceId = parseInt(req.body.experienceId, 10);
-  const averageRating = parseInt(req.body.averageRating, 10);
-  const numberOfReviews = parseInt(req.body.numberOfReviews, 10);
-  const duration = parseInt(req.body.duration, 10);
-  const groupSize = parseInt(req.body.groupSize, 10);
-  const costPerPerson = parseInt(req.body.costPerPerson, 10);
-  const newExp = {
-    experienceId,
-    title: req.body.title,
-    city: req.body.city,
-    state: req.body.state,
-    country: req.body.country,
-    category: req.body.category,
-    activity: req.body.state,
-    averageRating,
-    numberOfReviews,
-    duration,
-    groupSize,
-    includes: req.body.includes,
-    cuisine: req.body.cuisine,
-    hostedLanguages: req.body.hostedLanguages,
-    costPerPerson,
-    imageUrls: req.body.imageUrls,
-    videoUrl: req.body.videoUrl,
-  };
-
-  db.updateExperience(newExp, (err, data) => {
+  db.updateExperience(buildNewExp(req), (err, data) => {
     if (err) {
       res.status(400).send({
         message: err.message,
